1d5e81dc7899ec9e12b8c12d4caeb3d865fe7037
unknown <luthfiananda100@gmail.com>
2019-08-30 20:02:50 +0700

    number 1 and 2

---
+++ [Luthfi]jawaban2/casef.py
@@ -0,0 +1,7 @@
+def case1(ljam,lmenit) :
+    for i in range(len(ljam)):
+        if (int(ljam[i])<=23) and (int(ljam[i])>=0) and (int(lmenit[i])>=0) and (int(lmenit[i])<=59) :
+            print(ljam[i]+':'+lmenit[i])
+
+
+    
